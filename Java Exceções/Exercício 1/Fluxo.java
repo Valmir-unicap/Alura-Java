77a36d4218d0cc761c0b1385e69c8760719e1d90
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-01-29 15:41:45 -0300

    Create Fluxo.java

---
+++ Java Exceções/Exercício 1/Fluxo.java
@@ -0,0 +1,28 @@
+public class Fluxo {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Inicio do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicio do metodo2");
+        for(int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            try { //bloco de código perigoso.
+                int a = 9;
+                int b = a / 0; //erro aritmético, não se pode dividir por zero.
+            }catch (ArithmeticException e){ //caso ocorre erro aritmético, ele vai executar o que está dentro do catch
+                System.out.println("Erro aritmético, você tentou dividir por zero.");
+            }
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
