6e116f67e09ee7832cdd26aaa09f95aed6030cf7
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-01-29 16:16:05 -0300

    Tratando duas exceções num único catch

---
+++ Java Exceções/Aula 5/Fluxo.java
@@ -0,0 +1,29 @@
+public class Fluxo {
+
+    public static void main(String[] args) {
+        System.out.println("Inicio do main");
+        try{
+            metodo1();
+        }catch (ArithmeticException | NullPointerException e){
+            String mensagem = e.getMessage();
+            System.out.println("Ocorreu erro de "+mensagem);
+        }
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Inicio do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Inicio do metodo2");
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+            Conta c = null;
+            c.depositar(); // Erro objeto vazio
+        }
+        System.out.println("Fim do metodo2");
+    }
+}
