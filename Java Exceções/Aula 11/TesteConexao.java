0e99bb63863e43227aa68caf352b0500baa22f24
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-01-29 23:29:45 -0300

    Update TesteConexao.java

--- Java Exceções/Aula 11/TesteConexao.java
+++ Java Exceções/Aula 11/TesteConexao.java
@@ -1,6 +1,6 @@
 public class TesteConexao {
     public static void main(String[]args){
-        try(Conexao conexao = new Conexao()){//ele abre e fecha conexão, pois está embutido no AutoCloseState. //try with resources
+        try(Conexao conexao = new Conexao()){//ele abre e fecha conexão, pois está embutido no AutoCloseState. //try with resources //tem o finally embutido
             conexao.leDados();
         }catch (Exception e){
             System.out.println("Erro de conecão!");
