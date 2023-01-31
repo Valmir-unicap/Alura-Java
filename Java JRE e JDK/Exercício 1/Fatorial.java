b5c0e85e462f2b758f471728f5df3e5a9ba335c5
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-01-31 00:42:24 -0300

    Create Fatorial.java

---
+++ Java JRE e JDK/Exercício 1/Fatorial.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class FluxoComTratamento {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Fatorial");
+        System.out.println("");
+        int fatorial = 1;
+        for (int i = 1; i < 20; i=i+1) {
+            fatorial = fatorial * i;
+            System.out.println("Fatorial de " + i + " = " + fatorial);
+        }
+    }
+}
