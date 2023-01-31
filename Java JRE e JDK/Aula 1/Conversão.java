f704f7188c21038c21c43eecb6bb513e11c48d4e
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-01-30 22:56:11 -0300

    Create Conversão.java

---
+++ Java JRE e JDK/Aula 1/Conversão.java
@@ -0,0 +1,8 @@
+public class Conversao {
+
+    public static void main(String[] args) {
+        double real= 1250.50;
+        int conversao = (int) real;
+        System.out.println(conversao);
+    }
+}
